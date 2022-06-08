c856a66052b4788072ef5ca0db9a5f85308b99c9
sarthakvts <63893512+sarthakvts@users.noreply.github.com>
2022-06-08 22:03:12 +0530

    Created first react app.
    - used pokemon api v2.
    -used axios to get json data.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import Axios from 'axios';
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [pokemonName, setPokemonName] = useState("");
+  const [pokemon, setPokemon] = useState({
+    name: pokemonName,
+    species: '',
+    img: '',
+    hp: '',
+    attack: '',
+    defence: '',
+    type: ''
+  });
+  const searchPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response) => {
+      setPokemon({
+        name: pokemonName,
+        species: response.data.species.name,
+        img: response.data.sprites.front_default,
+        hp: response.data.stats[0].base_stat,
+        attack: response.data.stats[1].base_stat,
+        defence: response.data.stats[2].base_stat,
+        type: response.data.types[0].type.name
+      });
+    });
+  };
 
+
+  return (
+    <div className='App'>
+      <div className='titleSection'>
+        <h1>
+          Pokedex
+        </h1>
+        <input type='text' placeholder='Enter Pokemon Name' onChange={(event) => { setPokemonName(event.target.value) }}></input>
+        <button type="submit" onClick={searchPokemon}>Search Pokemon</button>
+      </div>
+
+      <div className='displaySection'>
+
+        <h2>{pokemon.name.toUpperCase()}</h2>
+        <img src={pokemon.img}></img>
+        <h3>Species: {pokemon.species.toUpperCase()}</h3>
+        <h3>Type: {pokemon.type.toUpperCase()}</h3>
+        <h3>HP: {pokemon.hp}</h3>
+        <h3>Attack: {pokemon.attack}</h3>
+        <h3>Defence: {pokemon.defence}</h3>
+
+      </div>
+    </div >
+  )
+
+
+}
 export default App;
