3d81df564b71f6467f90630f054ff286fb2165bd
sarthakvts <63893512+sarthakvts@users.noreply.github.com>
2022-06-09 10:19:59 +0530

    SEARCH BAR UPDATED
    ADDED SEARCH PREDICTION TO THE SEARCH BAR

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
+import userEvent from '@testing-library/user-event';
+import axios from 'axios';
 import Axios from 'axios';
-import { useState } from 'react';
+import './custom.css';
+import { useEffect, useState } from 'react';
 
 import './App.css';
 
 function App() {
-  const [pokemonName, setPokemonName] = useState("");
+  const [text, setText] = useState('');
+  const [hints, setHint] = useState([]);
+
+  const [suggestions, setSuggestions] = useState([]);
+  useEffect(() => {
+    const loadHint = async () => {
+      const response = await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=1126&offset=0');
+      setHint(response.data.results);
+    }
+    loadHint();
+  }, []);
+  const onSuggestHandler = (text) => {
+    setText(text);
+    setSuggestions([]);
+  }
+  const onHandler = (text) => {
+    let matches = [];
+    if (text.length) {
+      matches = hints.filter(hint => {
+        const regex = new RegExp(`${text}`, 'gi');
+        return hint.name.match(regex)
+      }
+      )
+    }
+    console.log('matches', matches)
+    setSuggestions(matches)
+    setText(text)
+  }
   const [pokemon, setPokemon] = useState({
-    name: pokemonName,
+    name: text,
     species: '',
     img: '',
     hp: '',
@@ -14,10 +44,11 @@ function App() {
     defence: '',
     type: ''
   });
+
   const searchPokemon = () => {
-    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response) => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${text}`).then((response) => {
       setPokemon({
-        name: pokemonName,
+        name: text,
         species: response.data.species.name,
         img: response.data.sprites.front_default,
         hp: response.data.stats[0].base_stat,
@@ -35,16 +66,22 @@ function App() {
         <h1>
           Pokedex
         </h1>
-        <input type='text' placeholder='Enter Pokemon Name' onChange={(event) => { setPokemonName(event.target.value) }}></input>
+        <input type='text' onChange={e => onHandler(e.target.value)} value={text}
+          onblur={() => { setTimeout(() => { setSuggestions([]) }, 100); }}
+          placeholder='Enter Pokemon Name' ></input>
+        {suggestions && suggestions.map((suggestion, i) =>
+          <div key={i} className='suggestion' onClick={() => onSuggestHandler(suggestion.name)}>
+            {suggestion.name}
+          </div>)}
         <button type="submit" onClick={searchPokemon}>Search Pokemon</button>
       </div>
 
       <div className='displaySection'>
 
-        <h2>{pokemon.name.toUpperCase()}</h2>
+        <h2>{text}</h2>
         <img src={pokemon.img}></img>
-        <h3>Species: {pokemon.species.toUpperCase()}</h3>
-        <h3>Type: {pokemon.type.toUpperCase()}</h3>
+        <h3>Species: {pokemon.species}</h3>
+        <h3>Type: {pokemon.type}</h3>
         <h3>HP: {pokemon.hp}</h3>
         <h3>Attack: {pokemon.attack}</h3>
         <h3>Defence: {pokemon.defence}</h3>
